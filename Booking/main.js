37e2f76dc06c4e6efabd8215e19d58ee10344e9e
Njomeza Nasufi <njnasufi@gmail.com>
2020-02-16 21:52:09 +0100

    Initial commit

---
+++ Booking/main.js
@@ -0,0 +1,48 @@
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(p => {
+  p.addEventListener("click", () => {
+    selected.innerHTML = p.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+
+const selected1 = document.querySelector(".selected1");
+const optionsContainer1 = document.querySelector(".options-container1");
+
+const optionsList1 = document.querySelectorAll(".option1");
+
+selected1.addEventListener("click", () => {
+  optionsContainer1.classList.toggle("active");
+});
+
+optionsList1.forEach(o => {
+  o.addEventListener("click", () => {
+    selected1.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer1.classList.remove("active");
+  });
+});
+
+
+const selected2 = document.querySelector(".selected2");
+const optionsContainer2 = document.querySelector(".options-container2");
+
+const optionsList2 = document.querySelectorAll(".option2");
+
+selected2.addEventListener("click", () => {
+  optionsContainer2.classList.toggle("active");
+});
+
+optionsList2.forEach(q => {
+  q.addEventListener("click", () => {
+    selected2.innerHTML = q.querySelector("label").innerHTML;
+    optionsContainer2.classList.remove("active");
+  });
+});
